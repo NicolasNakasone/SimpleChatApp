e0edafb16b0dbdbd20ae6bd5ee34c63167222b29
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-10-29 21:53:32 -0300

    feat(folder): created client folder

---
+++ client/vite.config.ts
@@ -0,0 +1,13 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+      public: path.resolve(__dirname, 'public'),
+    },
+  },
+})
