86787094579a3025b59d98b8fffcd9d2130aaf8c
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-10-31 11:59:52 -0300

    feat(socket): installed socket.io config
    * It have been added the first events to listen and emit information

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import server from 'src/app'
+import server, { app } from 'src/app'
 
-server.listen(server.get('port'), () => {
+server.listen(app.get('port'), () => {
   console.log(`Listening at port ${process.env.API_PORT}!`)
 })
--- api/src/app.ts
+++ api/src/app.ts
@@ -1,17 +1,37 @@
+import { createServer } from 'node:http'
+import { join } from 'path'
+
 import dotenv from 'dotenv'
 import express from 'express'
 import logger from 'morgan'
+import { Server } from 'socket.io'
 
 dotenv.config()
 
-const server = express()
 const { API_PORT } = process.env
 
-server.set('port', API_PORT || 4001)
-server.use(logger('dev'))
+export const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
-server.get('/', (req, res) => {
-  res.send('<h1>SimpleChatApp</h1>')
+app.set('port', API_PORT || 4001)
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'))
+})
+
+io.on('connection', socket => {
+  console.log('A user has connected!')
+
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg)
+    console.log('Message: ', msg)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected')
+  })
 })
 
 export default server
